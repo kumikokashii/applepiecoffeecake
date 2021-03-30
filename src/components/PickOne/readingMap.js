0fa80d55ca2a0aa75eb7ac3cf347b2799a32a584
kumikokashii <kkashii@uyahoo.com>
2021-03-30 00:55:09 -0700

    add three readings

--- src/components/PickOne/readingMap.js
+++ src/components/PickOne/readingMap.js
@@ -6,22 +6,37 @@ export const readingMap = {
 
   1: {
     'image': 'reading001.jpeg',
-    'text': "Overall, there’s a feeling of a new journey starting – but you may be struggling to really get started on it. Challenges may be coming from fighting against the natural current and still dealing with little bitty disagreements and concerns. It is advised to accept the heartache, if any, and say goodbye to someone who may be a bit too controlling over you, especially around your creativity."
+    'text': "Overall, there’s a feeling of a new journey starting – but you may be struggling to really get started on it. You will face challenges fighting against the natural current and still dealing with little bitty disagreements and concerns. It is advised to accept the heartache, if any, and say goodbye to someone who may be a bit too controlling over you, especially around your creativity."
   },
 
   2: {
     'image': 'reading002.jpeg',
-    'text': "Overall, it’s starting to become more and more about your pure creativity rather than the material matters you’ve worked hard to accumulate. In making this shift, you may be concerned about losing praise and admiration from others, and also causing some breakups with your old ties. It is advised to keep moving forward with your gut feelings – there may be lots of ups and downs but don’t worry, you won’t have time to stop and think about them. Just keep going."
+    'text': "Overall, it’s starting to become more and more about your pure creativity rather than the material matters you’ve worked hard to accumulate. You may be concerned about losing praise and admiration from others, and also causing some breakups of your old ties. It is advised to keep moving forward with your gut feelings – there may be ups and downs but you’ll be too busy to stop and think about them anyway. Just keep going."
   },
 
   3: {
     'image': 'reading003.jpeg',
-    'text': "Overall, you are presented with an opportunity to listen into your own thoughts and feelings – while also becoming extra sensitive to what is going on around you. This may challenge your mental health, especially by making you overthink about something. It is advised to experience things like a child again – with simplicity and curiosity, and take one step at time – at your own pace."
+    'text': "Overall, you are presented with an opportunity to listen into your own thoughts and feelings – while also becoming extra sensitive to what is going on around you. This may challenge your mental health, especially by making you overthink. It is advised to be childlike – to experience everything with simplicity and curiosity again. Also, it is advised to take it one step at time – at your own pace."
   },
 
   4: {
     'image': 'reading004.jpeg',
-    'text': "Overall, you are going to deal with some conflicts that arise from differences between you and the outside – it may be one person or a group of people. This is to protect what you stand for – and not to come to an agreement. Challenge will persist as long as you seek an agreement as a resolution. It is advised to keep your hope close to you, and get rid of your ideal picture of what ‘home’ means to you."
-  }
+    'text': "Overall, you are going to deal with some conflicts that arise from differences between you and the outside – it may be one person or a group of people. This is to protect what you stand for – but not to come to an agreement. Challenge will persist as long as you try to reach an agreement with the other(s). It is advised to keep your hope close to you, and get rid of your ideal picture of what ‘home’ means to you."
+  },
+
+  5: {
+    'image': 'reading005.jpeg',
+    'text': "Overall, there’s a sense of something new starting, perhaps a new project. However, there may be some unfinished concerns that keep you haunted in the past, unable to freely move ahead. It is advised to view yourself as someone standing at the end of one’s current journey. It is also advised to juggle two things at once – perhaps starting your new project while simultaneously completing your current project."
+  },
+
+  6: {
+    'image': 'reading006.jpeg',
+    'text': "Overall, it’s time for you to go solo and stay away from the world around you for awhile – to get lost in time working on your important project, whatever it may be to you. Challenge is in finding stability, and letting go of your defense mechanism. It is advised to approach the situation with imagination but without expectations."
+  },
+
+  7: {
+    'image': 'reading007.jpeg',
+    'text': "Overall, you will be the emotional hub for people around you. Also, there’s an end of your old self to become your new self. You may struggle to let go of your creative control when things do not go your own way. It is advised to find your inner strength to process your disappointments regarding what did not work out for you against your wish."
+  },
 
 }
