bcfce1cd9daf6145ba43bc111dd6fc3bf8b75914
kumikokashii <kkashii@uyahoo.com>
2021-03-29 17:32:59 -0700

    add a few readings

--- src/components/PickOne/readingMap.js
+++ src/components/PickOne/readingMap.js
@@ -1,12 +1,12 @@
 export const readingMap = {
   0: {
-    'image': 'test.jpg',
-    'text': "Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. Lalala Test 0. "
+    'image': 'reading000.jpeg',
+    'text': "Overall, there’s a great sense of happiness shared with a group of people you feel close to. Challenge is in having a quiet moment of your own to reflect, and to fully recognize your part in the success of the group. It is advised to look in a long-term, and see things as an investment for your future rather than what you gain in a short-term."
   },
 
   1: {
-    'image': 'test.jpg',
-    'text': "Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. Tatata testing 1. "
+    'image': 'reading001.jpeg',
+    'text': "Overall, there’s a feeling of a new journey starting – but you may be struggling to really get started on it. Challenges may be coming from fighting against the natural current and still dealing with little bitty disagreements and concerns. It is advised to accept the heartache, if any, and say goodbye to someone who may be a bit too controlling over you, especially around your creativity."
   }
 
 }
